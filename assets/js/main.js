b5d3a8c4ac98dfeeb45f0dbd245e124e4c71cd88
Samiul Islam <samiulm19356@gmail.com>
2025-07-07 21:40:53 +0600

    Code Modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,28 @@
         /* active menu item when click */
 
 
-
+            $('.user__carousel').owlCarousel({
+                loop: true,
+                margin: 30,
+                nav: true,
+                dots: false,
+                autoplay: true,
+                navText: [
+                    '<span class="custom-owl-prev">&#10094;</span>',
+                    '<span class="custom-owl-next">&#10095;</span>'
+                ],
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    992: {
+                        items: 3
+                    }
+                }
+            });
 
 
         // aos animation js
